55e9b0fc992d148b618e4afd2098fa7fac70680d
Laurens Van Houtven <_@lvh.io>
2015-02-19 14:37:03 -0800

    Add a test

---
+++ test/shimmer/rules_test.clj
@@ -0,0 +1,11 @@
+(ns shimmer.rules-test
+  (:require [shimmer.rules :as r]
+            [clojure.test :refer :all]
+            [clojure.core.logic :as l]))
+
+(deftest match*-tests
+  (testing "exact match"
+    (is (r/match* {:request-method :post
+                   :headers {"host" "localhost"}}
+                  [{:request-method :post
+                    :headers {"host" "localhost"}}]))))
