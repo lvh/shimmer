1d0067d31e0797ddbfd661845b3f0ad18f7d6110
Laurens Van Houtven <_@lvh.io>
2015-02-19 14:35:32 -0800

    Add core.logic

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.logic "0.8.8"]
+                 [ring/ring-mock "0.2.0"]]
   :main ^:skip-aot shimmer.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
