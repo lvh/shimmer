65e02e48d3a6fd9c916f8261152fa972cfbdddc7
Laurens Van Houtven <_@lvh.io>
2015-02-19 14:37:07 -0800

    Make it pass

---
+++ src/shimmer/rules.clj
@@ -0,0 +1,17 @@
+(ns shimmer.rules
+  (:require [clojure.core.logic :as l]))
+
+(defn ^:private conds
+  "Like conde, but a function."
+  [goals]
+  (if (empty? goals)
+    l/fail
+    (l/conde [(first goals)]
+             [(conds (rest goals))])))
+
+(defn match*
+  "Checks if a request matches a set of features."
+  [req features]
+  (not= (l/run* [q]
+          (conds (map (partial l/featurec req) features)))
+        '()))
