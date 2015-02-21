b2b9e629a9574762d69867942d90d885fa653907
Laurens Van Houtven <_@lvh.io>
2015-02-20 22:06:17 -0800

    Update tests

--- test/shimmer/rules_test.clj
+++ test/shimmer/rules_test.clj
@@ -1,21 +1,24 @@
 (ns shimmer.rules-test
   (:require [shimmer.rules :as r]
             [clojure.test :refer :all]
-            [clojure.core.logic :as l]))
+            [clojure.core.logic :as l]
+            [ring.mock.request :as m]))
 
-(deftest match*-tests
-  (testing "exact match"
-    (is (r/match* {:request-method :post
-                   :headers {"host" "localhost"}}
-                  [{:request-method :post
-                    :headers {"host" "localhost"}}])))
-  (testing "one matching clause is enough"
-    (is (r/match* {:request-method :post
-                   :headers {"host" "localhost"}}
-                  [{:request-method :post
-                    :headers {"host" "localhost"}}
-                   {:sure :whatever :some :stuff}])))
-  (testing "no match"
-    (is (not (r/match* {:request-method :post
-                        :headers {"host" "localhost"}}
-                       [])))))
+(deftest match-tests
+  (testing "GET anywhere matches"
+    (are [req] (r/match req)
+         (m/request :get "/")
+         (-> (m/request :get "/whatever/xyz")
+             (m/header "X-Some-Header" "some header value")
+             (m/header "X-Some-Other-Header" "some other header value"))))
+  (testing "POST / match, but only if they have the right headers"
+    (are [req] (r/match req)
+         (-> (m/request :post "/")
+             (m/header "X-Some-Header" "the right header value"))
+         (-> (m/request :post "/")
+             (m/header "X-Some-Header" "the right header value")
+             (m/header "Yet-Another-Header" "doesn't affect the match")))
+    (are [req] (not (r/match req))
+         (m/request :post "/")
+         (-> (m/request :post "/")
+             (m/header "X-Some-Header" "not the right header value")))))
