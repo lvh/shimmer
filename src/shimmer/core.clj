64ceede949b29334d097464c1a89fb9de7f2ac89
Laurens Van Houtven <_@lvh.io>
2015-02-20 22:09:59 -0800

    Get rid of empty let

--- src/shimmer/core.clj
+++ src/shimmer/core.clj
@@ -6,18 +6,17 @@
 
 (defn build-handler
   [target-host target-port]
-  (let []
-    (fn [incoming-request]
-      (if (match (spy incoming-request))
-        (let [modified-request (-> incoming-request
-                                   (dissoc :scheme) ;; hack
-                                   (assoc :host target-host
-                                          :port target-port
-                                          :throw-exceptions false))]
-          (spy (request (spy modified-request))))
-        {:status 403 ;; Forbidden
-         :headers {"content-type" "text/plain"}
-         :body "Doesn't match!"}))))
+  (fn [incoming-request]
+    (if (match (spy incoming-request))
+      (let [modified-request (-> incoming-request
+                                 (dissoc :scheme) ;; hack
+                                 (assoc :host target-host
+                                        :port target-port
+                                        :throw-exceptions false))]
+        (spy (request (spy modified-request))))
+      {:status 403 ;; Forbidden
+       :headers {"content-type" "text/plain"}
+       :body "Doesn't match!"})))
 
 (defn -main
   "Start a shimmer server"
