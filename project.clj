c63df4a517d01ac57baa3d0202f7b35d99609819
Laurens Van Houtven <_@lvh.io>
2015-02-20 22:05:56 -0800

    Add aleph

--- project.clj
+++ project.clj
@@ -5,6 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.logic "0.8.8"]
+
+                 ;; HTTP magic
+                 [aleph "0.4.0-beta3"]
                  [ring/ring-mock "0.2.0"]]
   :main ^:skip-aot shimmer.core
   :target-path "target/%s"
